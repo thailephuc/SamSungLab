a6ee0e4bc483e4f4603d62905ad081c7fb2a1506
Achilles <Achilles@192.168.5.18>
2015-04-08 16:08:29 +0700

    demo hien thi

---
+++ src/javademo.java
@@ -0,0 +1,15 @@
+
+public class javademo {
+	public static void main(String args[]){
+		int a = 5;
+		double b = 96.97;
+		char c = 't';
+		boolean d = true;
+		
+		System.out.println("a  = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+	}
+
+}
